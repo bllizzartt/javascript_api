3837b167260a82583950b34fda87167d9378ec56
chase.haynes <chase.haynez@gmail.com>
2022-10-24 10:50:10 -0400

    push
    workkk

---
+++ index.js
@@ -0,0 +1,15 @@
+// let today_day = prompt("Did I go to the gym today?");
+
+// if(today_day = "yes") {
+//     console.log('yes');
+// }
+
+var dice = {
+    sides: 6,
+    roll: function () {
+      var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+      return randomNumber;
+    }
+  }
+  console.log(randomNumber);
+
